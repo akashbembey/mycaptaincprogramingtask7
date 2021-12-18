0fac6b6509cb21c1a30f4ea81b423d6368a5e591
Preeti Bembey <74343887+akashbembey@users.noreply.github.com>
2021-12-18 19:54:56 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int marks[5]={89,90,92,87,76};
+    int i=0;
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",marks[i]);
+    }
+    return 0;
+}
